2d761f4e23dab23aab4d597d4f7ddc15cd5bd176
joelmusakka <91203662+joelmusa@users.noreply.github.com>
2022-11-18 12:39:09 +0200

    new

---
+++ Ajax.js
@@ -0,0 +1,32 @@
+function Moi() {
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("GET", "https://www.finnkino.fi/xml/TheatreAreas/", true);
+  xmlhttp.send();
+
+  xmlhttp.onreadystatechange = function () {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+      // find myDiv and insert results there
+      parseXML(this);
+    }
+  };
+}
+function parseXML(xml) {
+  xmlData = xml.responseXML;
+  var x = xmlData.getElementsByTagName("Name");
+  var table = "<li>";
+  var i;
+  for (i = 2; i < x.length; i++) {
+    table += x[i].childNodes[0].nodeValue;
+    console.log(x[i].childNodes[0].nodeValue);
+  }
+  table += "</li>";
+  document.getElementById("cityList").innerHTML = table;
+}
+
+function dropDown() {
+  var ul = document.getElementById("cityList");
+  var li = document.createElement("li");
+
+  ul.appendChild(li);
+  li.appendChild(x[i].childNodes[0].nodeValue);
+}
